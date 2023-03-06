68cf27d63b52ed3564aab0f28d34a90b39ba23eb
GetRuddy-DF <37291767+GetRuddy-DF@users.noreply.github.com>
2023-03-07 03:47:49 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function slowScroll(id) {
+    $('html, body') .animate({
+        scrollTop: $(id).offset().top,
+        scrollServices: $(id).offset().services
+    }, 500);
+}
+
+
+$(document).on("scroll", function () {
+    if($(window).scrollTop() === 0)
+    $("header").removeClass("fixed");
+    else
+    $("header").attr("class","fixed");
+});
+
+document.querySelector('.humburger').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('#about').classList.toggle('open');
+})
